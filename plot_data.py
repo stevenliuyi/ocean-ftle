f9e984563ffd5d85ad66d39d963cad167bd9de29
stevenliuyi <stevenliuyi@gmail.com>
2014-11-05 02:19:12 -0500

    rewrite show_traj function

--- plot_data.py
+++ plot_data.py
@@ -130,31 +130,23 @@ def interp(x1, x2, y1, y2, xm):
     return ym
 
 # -----------------------------------------------------------------------------
-def show_traj():
-    c1 = 'y'; c2 = 'c'; c3='g'
-    #m.plot(-78.75, 29.625, 'o', linewidth=15, color=c3)
-    #m.plot(-79, 29.875, 'o', linewidth=15, color=c3)
-    #m.plot(-79.25, 30.125, 'o',linewidth=15,  color=c3)
-    #m.plot(-79.5, 30.375, 'o', linewidth=15, color=c3)
-    #m.plot(-79.75, 30.625, 'o', linewidth=15, color=c3)
-    #m.plot(-80.0, 30.875, 'o', linewidth=15, color=c3)
-
-    #45N/65W
-    #89/36
-    m.plot([-78.75, -78.6521, -78.5575, -78.4731, -78.3934, -78.3111, -78.2250, -78.1327, -78.0289, -77.9179, -77.8023, -77.6852], [29.625, 29.5528, 29.4939, 29.4622, 29.4557, 29.4692, 29.4882, 29.5049, 29.5147, 29.5055, 29.4763, 29.4449], linewidth=5, color=c2)
-    #87/38
-    m.plot([-79, -78.9294, -78.8497, -78.7630, -78.6741, -78.5792, -78.4865, -78.3942, -78.2948, -78.1844, -78.0620, -77.9336], [29.875, 29.8252, 29.7550, 29.6752, 29.6090, 29.5541, 29.5240, 29.5212, 29.5424, 29.5633, 29.5735, 29.5663],  linewidth=5, color=c1)
-    #85/40
-    m.plot([-79.25, -79.2566, -79.2722, -79.2972, -79.3295, -79.3605, -79.3771, -79.3568, -79.2665, -79.0993, -78.8312, -78.5274], [30.125, 30.1797, 30.2432, 30.3203, 30.4168, 30.5447, 30.7230, 30.9667, 31.2765, 31.6139, 31.9382, 32.1633], linewidth=5, color=c2)
-    #83/42
-    m.plot([-79.5, -79.5415, -79.5338, -79.4505, -79.2972, -79.0952, -78.9010, -78.7370, -78.5919, -78.4536, -78.3142, -78.1878], [30.375, 30.6112, 30.9256, 31.2982, 31.6681, 31.9684, 32.1725, 32.3035, 32.3855, 32.4510, 32.5186, 32.5849], linewidth=5, color=c1)
-    #81/44
-    m.plot([-79.75, -79.7402, -79.6523, -79.5316, -79.4024, -79.2750, -79.1646, -79.0790, -79.0138, -78.9649, -78.9251, -78.8866], [30.625, 31.0146, 31.3669, 31.6596, 31.8561, 32.0116, 32.1348, 32.2320, 32.3057, 32.3515, 32.3804, 32.3995], linewidth=5, color=c2)
-    #79/46
-    m.plot([-80.0, -79.9551, -79.8918, -79.8294, -79.7599, -79.6767, -79.5840, -79.4831, -79.3674, -79.2518, -79.1494, -79.0635], [30.875, 31.1319, 31.3184, 31.4560, 31.5623, 31.6530, 31.7470, 31.8428, 31.9427, 32.0564, 32.1510, 32.2348], linewidth=5, color=c1)
-    #77/48
-    #m.plot([-80.25, -80.2907, -80.3260, -80.3566, -80.3729, -80.3916, -80.4101, -80.4366, -80.4572, -80.4696, -80.5375, -80.6021], [31.125, 31.1049, 31.0985, 31.1086, 31.1207, 31.1425, 31.1912, 31.2565, 31.2524, 31.2480, 31.2504, 31.2621], linewidth=5, color=c2)
-
+def show_traj(nlon, nlat):
+    for i in range(0, nx):
+        for j in range(0, ny):
+            valid = True
+            if (i % nlon == 0) and (j % nlat == 0):
+                itrajx = []; itrajy = []
+                for t in range(0, 11):
+                    coorx = coord(t, i, j)[0]
+                    coory = coord(t, i, j)[1]
+                    if (np.isnan(coorx) or np.isnan(coory)):
+                        valid = False
+                        break
+                    itrajx.append(coorx)
+                    itrajy.append(coory)
+                if (valid):
+                    m.plot(itrajx[0], itrajy[0], 'o', mec='y', mfc='y', ms=2)
+                    m.plot(itrajx, itrajy, linewidth=1, color='y')
     return
 
 o_down_lat = 25.125
@@ -187,6 +179,8 @@ plot_type = "velocity"
 #plot_type = "vorticity"
 
 for t in range(6, 8, 4):
+    fig = plt.figure(figsize=(14,7))
+
     m = Basemap(projection='cyl',llcrnrlat=down_lat,urcrnrlat=up_lat,\
             llcrnrlon=left_lon,urcrnrlon=right_lon,resolution='i')
     m.drawcoastlines(linewidth=.5, color='#444444')
@@ -216,8 +210,6 @@ for t in range(6, 8, 4):
 
     # setup trajectory grid
     (x,y) = setup_grid(nx, ny, down_lat, up_lat, left_lon, right_lon)
-
-    fig = plt.figure(figsize=(14,7))
 
     # velocity field
     if (plot_type == 'velocity'):
@@ -266,7 +258,7 @@ for t in range(6, 8, 4):
     #im.set_clim(vmin=0.000004,vmax=0.00001)
 
     # trajectory
-    #show_traj()
+    show_traj(10, 10)
 
     plt.title("pFTLE, nFTLE and " + plot_type + " [Day " + str(t) + "]")
     #fig.savefig('ftle_' + plot_type + '_' + t_str + '.png')
