98c3a8e848777d1b6dac32c6d2456cd86b6bb8b9
stevenliuyi <stevenliuyi@gmail.com>
2014-11-06 17:32:20 -0500

    add condor submit file

--- submit.py
+++ submit.py
@@ -5,22 +5,22 @@ f.write('executable = ftle\n')
 f.write('should_transfer_files = YES\n')
 f.write('when_to_transfer_output = ON_EXIT\n\n')
 
-for t in range(6, 4946, 4):
+for t in range(7, 4945):
     for direction in ["pos", "neg"]:
         t_str = str(t).zfill(4)
         f.write('output = ftle_' + t_str + '_' + direction + '.out\n')
         f.write('error = ftle_' + t_str + '_' + direction + '.error\n')
         f.write('log = ftle_' + t_str + '_' + direction + '.log\n')
         trans = ''
-        for i in range(t-5, t+6):
+        for i in range(t-6, t+7):
             trans += 'data/u_' + str(i).zfill(4) + '.ascii,' + \
                      'data/v_' + str(i).zfill(4) + '.ascii,'
         trans = trans[:-1]
         f.write('transfer_input_files = ' + trans + '\n')
         if (direction == "pos"):
-            f.write('arguments = ' + str(t) + ' 1 3 .5 5 2 \n')
+            f.write('arguments = ' + str(t) + ' 1 5 .3 6 2 \n')
         if (direction == "neg"):
-            f.write('arguments = ' + str(t) + ' -1 3 .5 5 2 \n')
+            f.write('arguments = ' + str(t) + ' -1 5 .3 6 2 \n')
 
         f.write('queue\n\n')
 
